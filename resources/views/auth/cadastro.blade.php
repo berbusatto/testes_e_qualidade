179f6805ca8e5dea9cb9c892abf9e17e409fca04
Bernardo Busatto <berbusatto@gmail.com>
2023-03-25 15:40:04 -0300

    responsividade e validacao

--- resources/views/auth/cadastro.blade.php
+++ resources/views/auth/cadastro.blade.php
@@ -4,57 +4,55 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Login</title>
-
+    <title>Cadastro</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 </head>
 <body class="bg-dark">
+    <div class="container d-flex align-items-center justify-content-center vh-100">
+        <div class="border border-light rounded p-4 mx-auto col-sm-10 col-md-8 col-lg-6 col-xl-4">
+            <form method="post" action="{{route('cadastraUsuario')}}">
 
-    <form method="post" action="{{route('cadastraUsuario')}}">
-
-        <div class="border border-light rounded p-4 mx-auto" style="max-width: 600px;">
-            <br>
-            <p class="h2 text-center text-white">CADASTRE-SE</p>
-            <br>
-
-            <div class="form-group text-center">
-                @csrf
-                <div class="input-group mb-lg-1 mx-auto" style="width: 250px;">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text bg-dark text-white">Usuário</span>
-                    </div>
-                    <input type="text" name="username" class="form-control" aria-label="username">
+                <div>
+                    <br>
+                    <p class="h2 text-center text-white">CADASTRE-SE</p>
+                    <br>
                 </div>
 
-                <br><br>
-                <div class="input-group mb-lg-1 mx-auto" style="width: 250px;">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text bg-dark text-white">&ensp;Email&ensp;</span>
+                <div class="form-group text-center">
+                    @csrf
+                    <div class="input-group mb-sm-1 mx-auto d-flex w-100">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text bg-dark text-white">Usuário</span>
+                        </div>
+                        <input type="text" name="username" class="form-control" aria-label="username">
                     </div>
-                    <input type="text" name="email" class="form-control" aria-label="email">
-                </div>
-
-                <br><br>
-
-                <div class="input-group mb-lg-1 mx-auto" style="width: 250px;">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text bg-dark text-white">&ensp;Senha&ensp;</span>
+                    <br><br>
+                    <div class="input-group mb-sm-1 mx-auto d-flex w-100">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text bg-dark text-white">&ensp;Email&ensp;</span>
+                        </div>
+                        <input type="text" name="email" class="form-control" aria-label="email">
                     </div>
-                    <input type="password" class="form-control" aria-label="password" name="password">
+                    <br><br>
+                    <div class="input-group mb-sm-1 mx-auto d-flex w-100">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text bg-dark text-white">&ensp;Senha&nbsp;</span>
+                        </div>
+                        <input type="password" class="form-control" aria-label="password" name="password">
+                    </div>
+                    <br><br>
+
+                    {{--INSERIR MENSAGEM DE ERRO--}}
+
+                    <div>
+                        <a href=" {{route('login')}} " class="btn btn-outline-light mx-2">Fazer login</a>
+                        <input class="btn btn-outline-success mx-2" type="submit" value="Cadastrar">
+                    </div>
                 </div>
-
-                {{--                    INSERIR MENSAGEM DE ERRO--}}
-
-                <br>
-                <a href=" {{route('login')}} " class="btn btn-outline-light">Fazer login</a>
-                <input class="btn btn-outline-success" type="submit" value="Cadastrar">
-
-
-
-            </div>
+            </form>
         </div>
-    </form>
+    </div>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,55 +3,56 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>Login</title>
-
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
 </head>
 <body class="bg-dark">
-
-    <form method="post" action="{{route('verificaLogin')}}">
-
-        <div class="border border-light rounded p-4 mx-auto" style="max-width: 600px;">
-            <br>
-            <p class="h2 text-center text-white">LOGIN</p>
-            <br>
+<div class="container d-flex align-items-center justify-content-center vh-100">
+    <div class="border border-light rounded p-4 mx-auto col-sm-10 col-md-8 col-lg-6 col-xl-4">
+        <form method="post" action="{{route('verificaLogin')}}">
+            <div>
+                <br>
+                <p class="h2 text-center text-white">LOGIN</p>
+                <br>
+            </div>
 
             <div class="form-group text-center">
                 @csrf
-                <div class="input-group mb-lg-1 mx-auto" style="width: 250px;">
+                <div class="input-group mb-sm-1 mx-auto d-flex w-100">
                     <div class="input-group-prepend">
-                        <span class="input-group-text bg-dark text-white">&nbsp;Email&nbsp;</span>
+                        <span class="input-group-text bg-dark text-white">&ensp;Email&ensp;</span>
                     </div>
-                    <input type="text" name="email" class="form-control" aria-label="email">
+                    <input type="text" name="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" aria-label="email" value="{{ old('email') }}">
+                    @if ($errors->has('email'))
+                        <div class="invalid-feedback">
+                            {{ $errors->first('email') }}
+                        </div>
+                    @endif
+
                 </div>
                 <br><br>
-                <div class="input-group mb-lg-1 mx-auto" style="width: 250px;">
+                <div class="input-group mb-sm-1 mx-auto d-flex w-100">
                     <div class="input-group-prepend">
-                        <span class="input-group-text bg-dark text-white">&nbsp;Senha&nbsp;</span>
+                        <span class="input-group-text bg-dark text-white">&ensp;Senha&ensp;</span>
                     </div>
-                    <input type="password" class="form-control" aria-label="password" name="password">
-                </div>
-
-                <div>
-                    {{--                     INSERIR MENSAGEM DE ERRO--}}
-                    @if (session()->has('mensagemErro'))
-                        <br>
-                        @foreach(session('mensagemErro') as $erro)
-                            <div class="alert alert-danger mx-auto" style="width: 250px;">{{ $erro }}</div>
-                        @endforeach
-
+                    <input type="password" name="password" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" aria-label="password" >
+                    @if ($errors->has('password'))
+                        <div class="invalid-feedback">
+                            {{ $errors->first('password') }}
+                        </div>
                     @endif
+
                 </div>
-
-                <br>
-                <a href="{{route('geraFormulario')}}" class="btn btn-outline-light">Cadastrar</a>
-                <input class="btn btn-outline-success" type="submit" value="Entrar">
-
+                <br><br>
+                <div class="text-center">
+                    <a class="btn btn-outline-light mx-2" href="{{route('geraFormulario')}}">Cadastrar</a>
+                    <input class="btn btn-outline-success mx-2" type="submit" value="Entrar">
+                </div>
+                <br><br>
             </div>
-        </div>
-    </form>
+        </form>
+    </div>
+</div>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
